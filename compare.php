f3109dc4632d3a4faee0ac355483ba0782975a82
Shahlal Hossain <shahlalhossain@gmail.com>
2015-03-20 12:49:43 +0600

    Number Comparison Update

--- compare.php
+++ compare.php
@@ -1,29 +1,40 @@
-
 <html>
+<head>
+    <title>Compare 2 Numbers</title>
+</head>
 <body>
-<form action="<?php $_PHP_SELF ?>" method="POST">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
-    Input First Number: <input type="text" name="number1" /> <br>
-    Input Second Number: <input type="text" name="number2" />
-    <input type="submit" value="Compare" />
-</form>
+        First Number: <input type="text" name="number1" /> <br>
+        Second Number: <input type="text" name="number2" />
+        <input type="submit" value="Compare" />
+
+    </form>
 </body>
 </html>
 
-
-
 <?php
 
-$number1 = $_POST['number1']. "<br />";
-$number2 = $_POST['number2']. "<br />";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $number1 = $_POST['number1']. "<br />";
+    $number2 = $_POST['number2']. "<br />";
 
-if($number1 > $number2)
-{
-    echo "$number1" . " is greater than" . " $number2";
-}
-else
-{
-    echo "Two Number is Equal";
+    if ($number1 && $number2) {
+        echo ("Number1: " . $number1);
+        echo ("Number2: " . $number2);
+
+        if ((int)$number1 > (int)$number2) {
+            echo "First Number is greater than Second Number";
+        }
+
+        if ((int)$number1 < (int)$number2) {
+            echo "First Number is less than Second Number";
+        }
+
+        if ((int)$number1 == (int)$number2) {
+            echo "Two Number is Equal";
+        }
+    }
 }
 ?>
